83f4c566c2491a9ba98d34e9b9f131ad7b417ee9
Null Ragnarok Relation <59070443+iberryRO@users.noreply.github.com>
2022-10-12 03:18:07 -0700

    Delete serverupdate.bat

--- serverupdate.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-Title Automatic update the latest Git from the rAyam by.null#6385
-ECHO Getting the latest Git.
-ECHO please wait...
-git pull https://github.com/iberryRO/Evo.git
-timeout /t 3
-cls
-Echo ---------------------------------------------------
-Set /p Wait="Finished."
